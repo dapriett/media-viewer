5123e232f658557a3234f8ad6eb15095685d2b95
velimd <velican.duzgunce@solirius.com>
2019-06-27 14:03:30 +0100

    including method to make div movable.

--- projects/media-viewer/src/lib/annotations/annotation-set/annotation/rectangle/rectangle.component.ts
+++ projects/media-viewer/src/lib/annotations/annotation-set/annotation/rectangle/rectangle.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { Rectangle } from './rectangle.model';
 import { IResizeEvent } from 'angular2-draggable/lib/models/resize-event';
 
@@ -9,12 +9,17 @@ import { IResizeEvent } from 'angular2-draggable/lib/models/resize-event';
 })
 export class RectangleComponent {
 
+  @ViewChild('rectangleDiv') rectangleDiv: ElementRef;
   @Input() selected: boolean;
   @Input() rectangle: Rectangle;
   @Input() color: String;
   @Input() zoom: number;
   @Input() rotate: number;
   @Input() draggable = true;
+  x: number;
+  y: number;
+  left: string;
+  top: string;
 
   @Output() click = new EventEmitter();
   @Output() update = new EventEmitter<Rectangle>();
@@ -23,19 +28,60 @@ export class RectangleComponent {
     this.click.emit();
   }
 
-  onMove(el: HTMLElement) {
-    const [left, top] = el.style.transform.match(/\d+/g);
+  onMouseDown(e) {
+    console.log('selected');
+    this.selected = true;
+    const coordinates = this.getTrueCoordinates(e.x, e.y);
+    this.x = coordinates.x;
+    this.y = coordinates.y;
 
-    this.rectangle.x = +left / this.zoom;
-    this.rectangle.y = +top / this.zoom;
-
-    this.update.emit(this.rectangle);
+    document.addEventListener('mousemove', (event) => {
+      this.onMouseMove(event);
+    });
+    document.addEventListener('mouseup', () => {
+      this.onMouseUp();
+    });
   }
 
-  onResize(event: IResizeEvent) {
-    this.rectangle.width = event.size.width / this.zoom;
-    this.rectangle.height = event.size.height / this.zoom;
+  onMouseMove(e) {
+    if (this.selected) {
+      const coordinates = this.getTrueCoordinates(e.x, e.y);
+      this.top = coordinates.y + 'px';
+      this.left = coordinates.x + 'px';
+    }
+  }
 
-    this.update.emit(this.rectangle);
+  onMouseUp() {
+    console.log('unselected');
+    this.selected = false;
+  }
+
+  getTrueCoordinates(x, y) {
+    let offsetX = 0;
+    let offsetY = 0;
+
+    let element = this.rectangleDiv.nativeElement.offsetParent;
+    const stylePaddingLeft = parseInt(document.defaultView.getComputedStyle(element, null)['paddingLeft'], 10)     || 0;
+    const stylePaddingTop  = parseInt(document.defaultView.getComputedStyle(element, null)['paddingTop'], 10)      || 0;
+    const styleBorderLeft  = parseInt(document.defaultView.getComputedStyle(element, null)['borderLeftWidth'], 10) || 0;
+    const styleBorderTop   = parseInt(document.defaultView.getComputedStyle(element, null)['borderTopWidth'], 10)  || 0;
+
+    if (element.offsetParent) {
+      do {
+        offsetX += element.offsetLeft;
+        offsetY += element.offsetTop;
+      } while ((element = element.offsetParent));
+    }
+    // Add padding and border style widths to offset
+    offsetX += stylePaddingLeft;
+    offsetY += stylePaddingTop;
+
+    offsetX += styleBorderLeft;
+    offsetY += styleBorderTop;
+
+    return {
+      x: (x + scrollX) - offsetX,
+      y: (y + scrollY) - offsetY,
+    }
   }
 }
